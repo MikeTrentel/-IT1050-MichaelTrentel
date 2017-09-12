d09defdb8496fddc16b56a14553ed863884c751b
MikeTrentel <s01165465@acad.tri-c.edu>
2017-09-11 23:57:27 -0400

    Lab02-MichaelTrentel

---
+++ Lab02/Lab02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    class Addition
+    {
+        static void Main()
+        {
+            int number1;
+            int number2;
+            int sum;
+
+            Console.Write("Enter first integer: ");
+            number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second integer: ");
+            number2 = int.Parse(Console.ReadLine());
+
+            sum = number1 + number2;
+
+            Console.WriteLine($"Sum is {sum}");
+            Console.ReadKey();
+
+            int x;
+            int y;
+            int z;
+
+            Console.Write("Enter x value: ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter y value: ");
+            y = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter z value: ");
+            z = int.Parse(Console.ReadLine());
+
+            sum = (x + y) * (z + 10);
+
+            Console.WriteLine($"Answer is {(x + y)*(z + 10)}");
+            Console.ReadKey();
+
+            Console.WriteLine("Hello\tWorld");
+            Console.ReadKey();
+
+            Console.WriteLine("Hello\nWorld");
+            Console.ReadKey();
+
+            Console.WriteLine("\"Hello World\"");
+            Console.ReadKey();
+
+            Console.WriteLine("Hello\\World");
+            Console.ReadKey();
+        }
+    }
+}
